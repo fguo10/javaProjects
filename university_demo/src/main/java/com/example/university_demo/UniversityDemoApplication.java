6c92a154c6974fba756149056a1a00a6ea7797fc
FanGuo <fguo10@hawk.iit.edu>
2023-06-14 14:43:42 -0500

    init university demo

---
+++ university_demo/src/main/java/com/example/university_demo/UniversityDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.university_demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UniversityDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UniversityDemoApplication.class, args);
+    }
+
+}
---
+++ university_demo/src/test/java/com/example/university_demo/UniversityDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.university_demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UniversityDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
